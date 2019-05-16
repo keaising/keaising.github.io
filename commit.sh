01e913a802974da45f3f96fc18019d838a14b2c4
keaising <keaising@gmail.com>
2019-05-16 21:12:06 +0800

    commit site at 2019-05-16

---
+++ commit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -e "\033[0;32mCommit updates to GitHub...\033[0m"
+
+# Go To src/gen folder
+cd src/gen
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="commit gen at `date +%F`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin gh-pages
+
+# Come Back up to the Project Root
+cd ../..
+
+git add .
+
+msg="commit site at `date +%F`"
+git commit -m "$msg"
+
+git push origin master
+
+echo -e "\033[0;32mDone!\033[0m"
