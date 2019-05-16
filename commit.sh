763de5fc5ab1583ed726a7598ac25efee5b51130
keaising <keaising@gmail.com>
2019-05-16 21:38:51 +0800

    commit gen at 2019-05-16T21:38:51

--- commit.sh
+++ commit.sh
@@ -2,30 +2,28 @@
 
 echo -e "\033[0;32mCommit updates to GitHub...\033[0m"
 
-# Go To src/gen folder
+# src/gen 
+
+echo -e "\033[0;32mCommit src/gen...\033[0m"
 cd src/gen
-
 git checkout master
-
-# Add changes to git.
 git add .
 
-# Commit changes.
-msg="commit gen at `date +%F`"
+msg="commit gen at `date +%FT%H:%M:%S`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
+git commit -m "$msg"
 git push origin master
 
 # Come Back up to the Project Root
+echo -e "\033[0;32mCommit root...\033[0m"
 cd ../..
 
 git add .
 
-msg="commit site at `date +%F`"
+msg="commit gen at `date +%FT%H:%M:%S`"
 git commit -m "$msg"
 
 git push origin master
