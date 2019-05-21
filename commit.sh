815f80e83313b7f8d3f81b7f454b392788418a2e
keaising <keaising@gmail.com>
2019-05-21 10:55:32 +0800

    commit blog at 2019-05-21T10:55:32

--- commit.sh
+++ commit.sh
@@ -7,7 +7,7 @@ echo -e "\033[0;32mCommit updates to GitHub...\033[0m"
 echo -e "\033[0;32mCommit src/gen...\033[0m"
 cd src/gen
 git checkout master
-git add .
+git add -A
 
 msg="commit gen at `date +%FT%H:%M:%S`"
 if [ $# -eq 1 ]
@@ -15,17 +15,19 @@ if [ $# -eq 1 ]
 fi
 
 git commit -m "$msg"
+git pull origin master -r
 git push origin master
 
 # Come Back up to the Project Root
 echo -e "\033[0;32mCommit root...\033[0m"
 cd ../..
 
-git add .
+git add -A
 
 msg="commit blog at `date +%FT%H:%M:%S`"
 git commit -m "$msg"
 
+git pull origin master -r
 git push origin master
 
 echo -e "\033[0;32mDone!\033[0m"
