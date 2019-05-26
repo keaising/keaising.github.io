8b4770475fcd3c507964104e7d0c61d99089f930
keaising <keaising@gmail.com>
2019-05-26 15:50:17 +0800

    commit blog at 2019-05-26T15:50:17

--- script/resync.sh
+++ script/resync.sh
@@ -5,3 +5,14 @@ cd ..
 
 git submodule sync
 git submodule update --init --recursive --remote
+
+cd public
+git checkout master
+
+cd ..
+cd src/gen
+git checkout master
+
+cd ../..
+cd themes/hugo-primer
+git checkout master
